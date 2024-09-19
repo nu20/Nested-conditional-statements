b1e05db6dca2c2c11542b1a403283b9ab55b82f1
nu20 <bibhutipatra123@gmail.com>
2024-09-19 18:11:15 +0530

    first commit

---
+++ Main.py
@@ -0,0 +1,12 @@
+attendence=int(input("enter your attendence :"))
+if attendence >75 :
+    print("you are allowed to take the exam.")
+else :
+    #assign value = 
+    #compare value ==
+    medicalcertificate=input("Do you have a medical certificate : ")
+    if medicalcertificate== "yes":
+     print("you are allowed to take the exam")
+    else:
+     print("you are not allowed to take the exam")
+
